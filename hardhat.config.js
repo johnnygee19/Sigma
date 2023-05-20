e2424d2fdc63655bb0b02fab1f1dd3998539abc9
Johnny Gee <sigmamining19@gmail.com>
2023-05-20 13:40:56 -0700

    Fix failing tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.4.24",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
 };
--- test/Sigma.js
+++ test/Sigma.js
@@ -18,13 +18,27 @@ describe("Sigma smart contract", function() {
   });
 
   describe("Deployment", function () {
-    it("Should make sure the owner is the one who deploys the contract.", async function () {
+    it("Should make sure the owner is the one who deploys the contract", async function () {
       expect(await sigma.owner()).to.equal(owner.address);
     });
 
-    it("Should make sure the tokens minted so far (total supply) are equal to the owner's balance who is the minter.", async function () {
+    it("Should make sure the tokens minted so far (total supply) are equal to the owner's balance who is the minter", async function () {
       const ownerBalance = await sigma.balanceOf(owner.address);
+      /*
+      This line of code is not working now for whatever reason (was working):
       expect(await sigma.totalSupply()).to.equal(ownerBalance);
+      Am getting:
+
+      AssertionError: expected { Object (_hex, _isBigNumber) } to equal { Object (_hex, _isBigNumber) }
+
+      Not sure if Chai/JavaScript supports BigNumber from Ethers.js so you have to convert it
+      to Number which is supported by Chai/JavaScript.
+
+      I think the root cause is because since wei is such a huge number, you have to convert it into ETH
+      with utils.formatEther (utils.formatEther(wei) => string) and compare the strings or turn the 
+      strings into Number objects and compare them that way.
+      */ 
+      expect(Number(hre.ethers.utils.formatEther(await sigma.totalSupply()))).to.equal(Number(hre.ethers.utils.formatEther(ownerBalance)));
     });
     /*
     AssertionError: expected 8e-11 to equal 80000000
@@ -33,7 +47,7 @@ describe("Sigma smart contract", function() {
       -8e-11
       +80000000
     */
-    it("Should make sure the maximum supply was set correctly after the deployment.", async function () {
+    it("Should make sure the maximum supply was set correctly after the deployment", async function () {
       const capacityAfterDeployment = await sigma.capacity();
       expect(Number(hre.ethers.utils.formatEther(capacityAfterDeployment))).to.equal(capacity);
     });
@@ -45,39 +59,42 @@ describe("Sigma smart contract", function() {
       -4e-17
       +40
     */
-    it("Should make sure the block reward was set correctly after the deployment.", async function () {
+    it("Should make sure the block reward was set correctly after the deployment", async function () {
       const blockRewardAfterDeployment = await sigma.blockReward();
       expect(Number(hre.ethers.utils.formatEther(blockRewardAfterDeployment))).to.equal(blockReward);
     });
   });
 
   describe("Transactions", function () {
-    it("Should transfer tokens between accounts.", async function () {
+    it("Should transfer tokens between accounts", async function () {
       // Transfer 40 tokens from owner to address1
       await sigma.transfer(address1.address, 40);
+      // AssertionError: expected BigNumber { value: "40" } to equal 40
       const balance1 = await sigma.balanceOf(address1.address);
-      expect(balance1).to.equal(40);
+      // console.log(hre.ethers.BigNumber.isBigNumber(balance1));
+      expect(balance1.toNumber()).to.equal(40);
 
       // Transfer 40 tokens from address1 to address2
       // We use .connect(signer) to send a transaction from another account
       await sigma.connect(address1).transfer(address2.address, 40);
       const balance2 = await sigma.balanceOf(address2.address);
-      expect(balance2).to.equal(40);
+      expect(balance2.toNumber()).to.equal(40);
     });
 
-    it("Should fail if the sender doesn't have enough tokens.", async function () {
+    it("Should fail if the sender doesn't have enough tokens", async function () {
       const initialOwnerBalance = await sigma.balanceOf(owner.address);
       // Try to send 1 token from address1 (0 tokens) to owner (1000000 tokens).
       // `require` will return a boolean value of false and revert the transaction.
+      // Error: Invalid Chai property: revertedWith
       await expect(
         sigma.connect(address1).transfer(owner.address, 1)
-      ).to.be.reverted;
+      ).to.be.revertedWith("Error: Transfer amount exceeds available balance.");
 
       // Owner's balance shouldn't have changed.
-      expect(await sigma.balanceOf(owner.address)).to.equal(initialOwnerBalance);
+      expect(hre.ethers.utils.formatEther(await sigma.balanceOf(owner.address))).to.equal(hre.ethers.utils.formatEther(initialOwnerBalance));
     });
 
-    it("Should update the balances after the transfers.", async function () {
+    it("Should update the balances after the transfers", async function () {
       const initialOwnerBalance = await sigma.balanceOf(owner.address);
 
       // Transfer 100 tokens from owner to address1.
@@ -88,13 +105,14 @@ describe("Sigma smart contract", function() {
 
       // Check the balances of owner, address1, and address2.
       const finalOwnerBalance = await sigma.balanceOf(owner.address);
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(140));
+      
+      expect(finalOwnerBalance.eq(initialOwnerBalance.sub(140))).to.equal(true);
 
       const balance1 = await sigma.balanceOf(address1.address);
-      expect(balance1).to.equal(100);
+      expect(balance1.toNumber()).to.equal(100);
 
       const balance2 = await sigma.balanceOf(address2.address);
-      expect(balance2).to.equal(40);
+      expect(balance2.toNumber()).to.equal(40);
     });
   });
 });
